699f0b101813eff246117af5e907efad001ac320
TheNewGuy2 <Gethermc@gmail.com>
2025-01-23 20:24:39 -0800

    Update index.js
    switch to 01

--- functions/index.js
+++ functions/index.js
@@ -37,7 +37,7 @@ async function generateTweet(prompt) {
     const response = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: 'o1-2024-12-17', // Updated model name gpt-4o
+        model: 'o1', // Updated model name gpt-4o
         messages: [
           { role: 'user', content: prompt },
         ],
